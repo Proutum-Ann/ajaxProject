a8372aa8cf2a661af2ad1fcc0f7893c02fcb9680
Proutum-Ann <liangproutum@gmail.com>
2025-10-09 11:51:21 -0400

    added baseline stuff (still doesn't work but yeah)

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 document.getElementById("load").addEventListener("click", function() {
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "friends.json", true);
+    xhr.open("GET", "mole.json", true);
     xhr.onload = function() {
+
         if (xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText);
-            document.getElementById("output").innerHTML = `placeholder`
+            const characterM = JSON.parse(xhr.responseText);
+
+            const cardContainer = document.getElementById('output')
+
+            cardContainer.innerHTML = ''
+
+            mole.forEach(characterM => {
+                const hobbyList = characterM.hobbies.map(
+                    hobby => `<span class="badge bg-info text-dark me-1">${hobby}</span>`
+                ).join('')
+
+                const cardHTML = `
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="card shadow-sm h-100">
+                        <div class="card-body">
+                            <h5 class="card-title">${characterM.name}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">${characterM.relationship}</h6>
+
+                            <p class="card-text">
+                                <b>Hobbies:</b><br> ${hobbyList}
+                            </p>
+
+                            <p class="card-text">Placeholder</p>
+                        </div>
+                    </div>
+                </div>
+                `
+            })
         }
     }
     xhr.send()

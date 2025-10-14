59491c3dbf02ea93997236a4bc195c9b34fb19e3
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-10-14 01:43:30 +0000

    idk at this point

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ document.getElementById("load").addEventListener("click", function() {
 
             const cardContainer = document.getElementById('output')
 
+            cardContainer.innerHTML = ''
+
             const cardHTML = document.createElement('div')
             cardHTML.classList = "col-12 col-md-4 col-lg-3"
 
             characterM.forEach(c => {
+
                 const hobbyList = c.hobbies.map(
                     hobby => `<span class="badge bg-info text-dark me-1">${hobby}</span>`
                 ).join('')
@@ -20,15 +23,22 @@ document.getElementById("load").addEventListener("click", function() {
                 cardHTML.innerHTML = `
                     <div class="card shadow-sm h-100">
                         <div class="card-body">
-                            <h5 class="card-title">${c.name}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">${c.role}</h6>
-                            <h6 class="card-subtitle mb-2 text-muted">${c.age}</h6>
-                            <p class="card-text">${c.aff.current}</p>
-                            <p class="card-text">${c.aff.past}</p>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <img src="${c.icon}">
+                                </div>
+                                <div class="col-md-8">
 
-                            <p class="card-text">
-                                <b>Hobbies:</b><br> ${hobbyList}
-                            </p>
+                                    <h5 class="card-title">${c.name}</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">${c.role}</h6>
+                                    <h6 class="card-subtitle mb-2 text-muted">${c.age}</h6>
+
+                                    <p class="card-text">
+                                        <b>Hobbies:</b><br> ${hobbyList}
+                                    </p>
+
+                                </div>
+                            </div>
 
                             <p class="card-text">${c.desc}</p>
                         </div>
@@ -37,6 +47,8 @@ document.getElementById("load").addEventListener("click", function() {
             })
 
             cardContainer.appendChild(cardHTML)
+        } else {
+            console.log("Error loading JSON file.");
         }
     }
     xhr.send()
